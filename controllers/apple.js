40810deb89c3d9b42571e3378789138b1ac50694
Swapna Dasari <45848234+DasariSwapna@users.noreply.github.com>
2021-11-12 01:49:14 -0600

    wrote put and get methods in controllers

--- controllers/apple.js
+++ controllers/apple.js
@@ -29,11 +29,17 @@ exports.apple_view_all_Page = async function (req, res) {
   
 
  
-// for a specific Apple. 
-exports.apple_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Apple detail: ' + req.params.id); 
+ // for a specific Apple. 
+exports.apple_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Apple.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
- 
 
  
 // Handle Apple delete form on DELETE. 
@@ -42,9 +48,32 @@ exports.apple_delete = function(req, res) {
 }; 
  
 // Handle Apple update form on PUT. 
-exports.apple_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Apple update PUT' + req.params.id); 
+exports.apple_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Apple.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.apple_type)  
+               toUpdate.apple_type = req.body.apple_type; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+        if(req.body.quantity) toUpdate.quantity = req.body.quantity; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
 }; 
+ 
+
+
+
+
+
+
 
 // Handle Costume create on POST. 
 exports.apple_create_post = async function(req, res) { 
